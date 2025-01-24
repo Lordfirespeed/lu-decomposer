465bac7951cd50a851491060c2d87f54be3988d5
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-01-24 11:04:27 +0000

    implement LU decomposer

---
+++ src/lu_decompose.py
@@ -0,0 +1,66 @@
+from typing import NamedTuple
+
+import numpy
+import numpy as np
+
+
+type Number = numpy.ScalarType
+type SquareMatrix[N] = np.matrix[(N, N), Number]
+
+
+class LUDecomposition[N](NamedTuple):
+    lower: SquareMatrix[N]
+    upper: SquareMatrix[N]
+
+
+def lu_decompose[N](matrix: SquareMatrix[N], lower: SquareMatrix[N] = None) -> LUDecomposition[N]:
+    assert len(matrix.shape) == 2
+    assert matrix.shape[0] == matrix.shape[1]
+    n: int = matrix.shape[0]
+    assert n > 0
+
+    if lower is None:
+        matrix = matrix.copy()
+        lower = np.asmatrix(np.identity(n, matrix.dtype))
+
+    if matrix[0, 0] != 1:
+        divider = matrix[0, 0]
+        matrix[0] /= divider
+        lower[0, 0] = divider
+
+    for row_index in range(1, n):
+        multiplier = matrix[row_index, 0]
+        if multiplier == 0:
+            continue
+
+        matrix[row_index] -= multiplier * matrix[0]
+        lower[row_index, 0] = multiplier
+
+    if n == 1:
+        return LUDecomposition(
+            lower,
+            matrix,
+        )
+
+    lu_decompose(matrix[1:, 1:], lower[1:, 1:])
+
+    return LUDecomposition(
+        lower,
+        matrix,
+    )
+
+
+if __name__ == "__main__":
+    my_matrix = numpy.asmatrix([
+        [3, -6, -3],
+        [2, 0, 6],
+        [-4, 7, 4]
+    ], dtype=float)
+    decomposition = lu_decompose(my_matrix)
+
+    print(decomposition.lower)
+    print(decomposition.upper)
+    print()
+    round_trip = decomposition.lower @ decomposition.upper
+    print(round_trip)
+    print((round_trip == my_matrix).all())
